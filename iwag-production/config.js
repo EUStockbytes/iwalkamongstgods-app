3e8eb1014ea49e8ef13afc580e2da2e7feb6baf8
EUStockbytes <eustockbytes@gmail.com>
2026-06-24 09:05:38 +0000

    Restore frontend config

--- iwag-production/config.js
+++ iwag-production/config.js
@@ -1,2 +1,2 @@
 window.SUPABASE_URL = "https://nqdbnepxzzgagiebvesm.supabase.co";
-window.SUPABASE_ANON_KEY = "PASTE_SUPABASE_ANON_KEY_HERE";
+window.SUPABASE_ANON_KEY = "sb_publishable_Gq8E1qiQoHzaT8h86lX0ow_j9vAb1pN";
